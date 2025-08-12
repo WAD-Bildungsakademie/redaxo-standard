b3a4a0c6fcebe12f3a11a78f0fe13c1d22a9a50a
shaack <shaack@gmail.com>
2025-08-12 22:16:50 +0200

    Hero with mask

--- web/redaxo/data/addons/developer/modules/Hero Big Picture [1]/output.php
+++ web/redaxo/data/addons/developer/modules/Hero Big Picture [1]/output.php
@@ -13,12 +13,17 @@ if($slice->getMedia(1)) {
 }
 $mask = $slice->getValue(6);
 ?>
-<h1><?= $slice->getValue(2) ?></h1>
-<p><?= $slice->getValue(3) ?></p>
-<!--
 <section class="module module-7bba0e mask-<?= $mask ?>">
     <div class="bg-image" style="background-image: url('<?= $url ?>')">
-
+        <div class="mask h-100">
+            <div class="container-fluid h-100 max-width-xxl mx-auto">
+                <div class="text-container h-100">
+                    <div class="text">
+                        <h1 class="display-3"><?= $slice->getValue(2) ?></h1>
+                        <div class="h3"><?= $slice->getValue(3) ?></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
--->
