892fbe4610564b91adecb4d422bacabc8d319208
shaack <shaack@gmail.com>
2025-08-26 22:03:15 +0200

    Sektion jetzt ohne Text

--- web/redaxo/data/addons/developer/modules/Sektion [2]/input.php
+++ web/redaxo/data/addons/developer/modules/Sektion [2]/input.php
@@ -3,8 +3,5 @@
 use FriendsOfRedaxo\MForm;
 
 $mForm = new MForm();
-$mForm->addTextField(1, ['label' => 'Text Überschrift']);
-$mForm->addTextField(2, ['label' => 'Text darunter']);
 $mForm->addTextField(9, ['label' => 'Text Navigation']);
-$mForm->addCheckboxField(8, ['label' => 'Getönter Hintergrund']);
 echo $mForm->show();
--- web/redaxo/data/addons/developer/modules/Sektion [2]/output.php
+++ web/redaxo/data/addons/developer/modules/Sektion [2]/output.php
@@ -6,19 +6,12 @@
 /** @var rex_article_slice $slice */
 $slice = $this->getCurrentSlice();
 $navigation = $slice->getValue(9);
-$bgGray = $slice->getValue(8);
 $id = EncryptionUtils::stringToHtmlId($navigation);
-$headline = $slice->getValue(1);
-$subheadline = $slice->getValue(2);
+if (rex::isBackend()) {
+    echo "Sektion \"" . $navigation . "\"";
+} else {
 ?>
-<section class="module module-0805ef <?= !$headline ? 'mb-0 pt-0' : '' ?> <?= $bgGray ? "bg-gray" : "" ?>">
+<section class="module module-0805ef mb-0 pt-0">
     <div class="anchor" <?= $navigation ? "id='" . $id . "'" : "" ?>></div>
-    <div class="container-fluid max-width-xl">
-        <?php if($headline) { ?>
-            <h2><?= $slice->getValue(1) ?></h2>
-            <div class="subheadline">
-                <?= $slice->getValue(2) ?>
-            </div>
-        <?php } ?>
-    </div>
 </section>
+<?php } ?>
